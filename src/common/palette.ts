25eb79691ad97fb494a24fe3a6cde6d12b7f6c5a
jhonnierandrey <jhonnierandrey@outlook.com>
2023-03-23 04:27:14 -0300

    AVS-crypto-list Assets and common media added.

---
+++ src/common/palette.ts
@@ -0,0 +1,13 @@
+const palette = {
+    blue: '#3E5772',
+    black: '#000000',
+    green: '#397E54',
+    grey: '#ABB0B5',
+    lightGray: '#E4E8EB',
+    red:'#CD452A',
+    white: '#FFFFFF',
+    yellow: '#F5D265',
+    lightYellow: '#D0B965',
+};
+
+export default palette;
