70f3c0a5865a1037ba254d6fe38bf3e275478e78
jhonnierandrey <jhonnierandrey@outlook.com>
2023-03-23 04:30:51 -0300

    JAZ-crypto-list updating entry component to use react navigation

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React, {Fragment} from 'react';
+import AppStack from './src/navigation/AppStack';
+
+const App = () => {
+  return (
+    <Fragment>
+      <AppStack />
+    </Fragment>
+  );
+};
+
+export default App;
---
+++ appStyles.ts
@@ -0,0 +1,9 @@
+import {StyleSheet} from 'react-native';
+import palette from './src/common/palette';
+
+export const appStyles = StyleSheet.create({
+    statusBar: {
+        flex: 0,
+        backgroundColor: palette.blue,
+    },
+});
