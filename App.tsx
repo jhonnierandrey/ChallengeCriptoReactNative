aa492831b19659273e2cefe8364a6dc387949126
jhonnierandrey <jhonnierandrey@outlook.com>
2023-03-24 12:29:07 -0300

    api-integration: wrapping app to use store provider

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import React, {Fragment} from 'react';
 import AppStack from './src/navigation/AppStack';
+import {Provider} from 'react-redux';
+import {store} from './src/redux/store';
 
 const App = () => {
   return (
     <Fragment>
-      <AppStack />
+      <Provider store={store}>
+        <AppStack />
+      </Provider>
     </Fragment>
   );
 };
