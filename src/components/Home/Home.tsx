abe597bf51165e9a62bdcb5447e2fea448f23cb8
jhonnierandrey <jhonnierandrey@outlook.com>
2023-03-24 12:32:45 -0300

    api-integration: <temporary> updating Home component to dispatch action that gets coin data from api

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import {useDispatch,useSelector} from 'react-redux';
 import {
     FlatList,
     Text,
@@ -10,48 +11,30 @@ import {
 import {homeStyles} from './homeStyles';
 
 import { Header, ListItem } from '../index';
+import { getCoinMetrics } from '../../redux/actions/cryptoActions';
 
-const hardcodedCryptos = [
-    {
-        id: '1e31218a-e44e-4285-820c-8282ee222035',
-        name: 'Bitcoin',
-        symbol: 'BTC',
-        market_data: {
-            price_usd: 7215.68,
-            percent_change_usd_last_24_hours: 1.82
-        }
-    },
-    {
-        id: '21c795f5-1bfd-40c3-858e-e9d7e820c6d0',
-        name: 'Ethereum',
-        symbol: 'ETH',
-        market_data: {
-            price_usd: 146.83,
-            percent_change_usd_last_24_hours: 1.46,
-        }
-    },
-    {
-        id: '97775be0-2608-4720-b7af-f85b24c7eb2d',
-        name: 'XRP',
-        symbol: 'XRP',
-        market_data: {
-            price_usd: 0.220568,
-            percent_change_usd_last_24_hours: -2.47,
-        }
-    },
-    {
-        id: 'c7c3697d-1b9c-42bf-9664-a366634ce2b3',
-        name: 'Litecoin',
-        symbol: 'LTC',
-        market_data: {
-            price_usd: 45.94,
-            percent_change_usd_last_24_hours: 1.47,
-        }
-    },
-];
+import { CryptoStore, Coin } from '../../types/types';
 
 const Home = ({navigation}) => {
-    const [selectedCryptos, setSelectedCryptos] = useState(hardcodedCryptos);
+    const dispatch = useDispatch();
+    
+    const {savedCryptos} = useSelector((store : CryptoStore) => store.crypto);
+    const [selectedCryptos, setSelectedCryptos] = useState<Coin[]>();
+    
+    useEffect(() =>{
+        if(savedCryptos.length > 0){
+            setSelectedCryptos(savedCryptos);
+        }else{
+            dispatch(getCoinMetrics('btc'));
+        }
+    }, []);
+    
+    useEffect(() => {
+        if (savedCryptos.length >= 1) {
+            setSelectedCryptos(savedCryptos);
+        }
+    }, [savedCryptos]);
+
     return (
         <>
             <SafeAreaView style={homeStyles.statusBar} />
@@ -60,12 +43,17 @@ const Home = ({navigation}) => {
                 <View style={homeStyles.main}>
                     <Header />
                     <View style={homeStyles.listContainer}>
-                        <FlatList
-                            data={selectedCryptos}
-                            keyExtractor={item => item.id}
-                            renderItem={({ item }) => <ListItem item={item} />}
-                        />
+                        {
+                            selectedCryptos && (
+                                <FlatList
+                                    data={selectedCryptos}
+                                    keyExtractor={item => item.id}
+                                    renderItem={({ item }) => <ListItem item={item} />}
+                                />
+                            )
+                        }
                     </View>
+                    {/* <Counter /> */}
                     <View style={homeStyles.addView}>
                         <TouchableOpacity onPress={() => { navigation.navigate('AddCoin') }}>
                             <Text style={homeStyles.btnAdd}>+ Add a Cryptocurrency</Text>
